fb562a8474e0c73079bcc01bddfca7d17ae70ecf
Rene Hexel <r.hexel@griffith.edu.au>
2017-04-15 06:55:26 +1000

    Add #include <sys/types.h>

--- cogl_bridging.h
+++ cogl_bridging.h
@@ -11,6 +11,7 @@ struct Display{};
 
 #define COGL_ENABLE_EXPERIMENTAL_API
 #define GLIB_DISABLE_DEPRECATION_WARNINGS
+#include <sys/types.h>
 #include <cogl/cogl.h>
 #ifndef __APPLE__
 #include <cogl/cogl-xlib.h>
